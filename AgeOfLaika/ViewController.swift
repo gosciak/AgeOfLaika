055c4e2ad7e2cc36c5fc5fd8d82418d9f4e9450b
Doug Gosciak <gosciak@mac.com>
2014-10-11 14:57:23 -0400

    Added all code to follow the video lectures

--- AgeOfLaika/ViewController.swift
+++ AgeOfLaika/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var enterHumanYearsTextField: UITextField!
+    @IBOutlet weak var dogYearsLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func convertToDogYearsButtonPressed(sender: UIButton) {
+        let humanYearsFromTextField = enterHumanYearsTextField.text.toInt()!
+        let dogYearsMultiplier = 7
+        dogYearsLabel.text = "\(humanYearsFromTextField * dogYearsMultiplier)" + " in Human Years"
+        enterHumanYearsTextField.text = ""
+        enterHumanYearsTextField.resignFirstResponder()
+        dogYearsLabel.hidden = false
+      
+        
+    }
 
 }
 
